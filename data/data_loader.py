5f8ffa6bcea7cd781cca9b458ed5490754cff045
Jeremyywb <gjeremyyou@gmail.com>
2023-05-25 16:08:14 +0800

    test informer add data loader2

--- data/data_loader.py
+++ data/data_loader.py
@@ -335,12 +335,12 @@ class Dataset_ETT_minute(Dataset):
         df_raw.columns: ['date', ...(other features), target feature]
         '''
         # cols = list(df_raw.columns); 
-        if self.cols:
-            cols=self.cols.copy()
-            cols.remove(self.target)
-        else:
-            cols = list(df_raw.columns); cols.remove(self.target); cols.remove('date')
-        df_raw = df_raw[['date']+cols+[self.target]]
+        # if self.cols:
+        #     cols=self.cols.copy()
+        #     cols.remove(self.target)
+        # else:
+        #     cols = list(df_raw.columns); cols.remove(self.target); cols.remove('date')
+        # df_raw = df_raw[['date']+cols+[self.target]]
 
         num_train = int(len(df_raw)*0.7)
         num_test = int(len(df_raw)*0.2)
